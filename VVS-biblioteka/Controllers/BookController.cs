561d5fc8d45c106dcd44e862dc19cd9e57042dfb
Ahmed Rascic <arascic1@etf.unsa.ba>
2023-12-12 18:28:44 +0100

    refactor code, request pr

--- VVS-biblioteka/Controllers/BookController.cs
+++ VVS-biblioteka/Controllers/BookController.cs
@@ -1,22 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System.Reflection;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
 using VVS_biblioteka.Models;
 
 namespace VVS_biblioteka.Controllers
 {
     [ApiController]
-    [Route("bookController")]
+    [Route("api/books")]
     public class BookController : ControllerBase
     {
         private readonly LibDbContext _context;
+        private readonly ILoanService _loanService;
 
-        public BookController(LibDbContext context) 
+        public BookController(LibDbContext context, ILoanService loanService)
         {
             _context = context;
+            _loanService = loanService;
         }
 
-        [HttpPost]
-        [Route("addBook")]
+        [HttpPost("add")]
         public async Task<IActionResult> AddBook(Book book)
         {
             if (!ModelState.IsValid)
@@ -35,8 +38,7 @@ namespace VVS_biblioteka.Controllers
             return Ok("Book added successfully!");
         }
 
-        [HttpGet]
-        [Route("getBookDetails/{bookId}")]
+        [HttpGet("{bookId}")]
         public IActionResult GetBookDetails(int bookId)
         {
             var book = _context.Book.FirstOrDefault(b => b.Id == bookId);
@@ -46,22 +48,16 @@ namespace VVS_biblioteka.Controllers
                 return NotFound($"Book with ID {bookId} not found.");
             }
 
-            var loan = _context.Loan.FirstOrDefault(l => l.BookId == bookId);
-
-            var user = loan != null ? _context.User.FirstOrDefault(u => u.Id == loan.UserId) : null;
-
             var bookDetails = new
             {
                 Book = book,
-                Loan = loan,
-                User = user
+                LoanDetails = _loanService.GetBookLoanDetails(bookId, _context)
             };
 
             return Ok(bookDetails);
         }
 
-        [HttpDelete]
-        [Route("deleteBook/{id}")]
+        [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteBook(int id)
         {
             var bookToDelete = await _context.Book.FindAsync(id);
@@ -76,74 +72,16 @@ namespace VVS_biblioteka.Controllers
 
             return Ok($"Book with ID {id} deleted successfully.");
         }
-        [NonAction]
-        public void ApplyCategorySpecificBenefits(User user, Loan loan)
-        {
-            switch (user.UserType)
-            {
-                case UserType.Student:
-                    loan.Price = CalculateDiscountedFee(loan.Price, 30);
-                    loan.Days = 60;
-                    break;
-                case UserType.Ucenik:
-                    loan.Price = CalculateDiscountedFee(loan.Price, 10);
-                    loan.Days = 15;
-                    break;
-                case UserType.Penzioner:
-                    loan.Price = CalculateDiscountedFee(loan.Price, 15);
-                    loan.Days = 30;
-                    break;
-                case UserType.Dijete:
-                    loan.Price = CalculateDiscountedFee(loan.Price, 5);
-                    loan.Days = 10;
-                    break;
-            }
-        }
 
-        private decimal CalculateDiscountedFee(decimal baseFee, int discountPercentage)
-        {
-            return baseFee - (baseFee * discountPercentage / 100);
-        }
-
-        [HttpPost]
-        [Route("/{loanBook}")]
+        [HttpPost("loan")]
         public async Task<IActionResult> LoanBook(LoanRequest request)
         {
-            Book book = _context.Book.FirstOrDefault(b => b.Id == request.BookId);
+            _loanService.LoanBook(request, _context);
 
-            var isBookLoaned = _context.Loan.Any(l => l.BookId == book.Id);
-
-            if (isBookLoaned)
-            {
-                throw new ArgumentException("Book is already loaned!");
-            }
-
-            Loan loan = _context.Loan.FirstOrDefault(l => l.UserId == request.UserId);
-
-            User user = _context.User.FirstOrDefault(u => u.Id == request.UserId);
-
-            if (loan != null)
-            {
-                throw new ArgumentException("You already loaned book!");
-            }
-
-            Loan l = new()
-            {
-                BookId = request.BookId,
-                UserId = request.UserId,
-                Date = DateTime.Today,
-                Price = book.price,
-            };
-
-            ApplyCategorySpecificBenefits(user, l);
-
-            _context.Loan.Add(l);
-            await _context.SaveChangesAsync();
             return Ok("You loaned a book successfully!");
         }
 
-        [HttpGet]
-        [Route("searchBooks")]
+        [HttpGet("search")]
         public IActionResult SearchBooks(string? title, string? author)
         {
             var books = _context.Book
@@ -155,20 +93,12 @@ namespace VVS_biblioteka.Controllers
             return Ok(books);
         }
 
-        [HttpDelete]
-        [Route("/{getBookBack}")]
+        [HttpDelete("return")]
         public async Task<IActionResult> GetBookBack(GetBookBackRequest request)
         {
-            Loan l=_context.Loan.FirstOrDefault(l => l.BookId == request.BookId);
-            if (l == null)
-            {
-                throw new ArgumentException("Book with that id is not loaned!");
-            }
-            _context.Loan.Remove(l);
+            _loanService.ReturnBook(request, _context);
 
-            await _context.SaveChangesAsync();
-            return Ok("You got book back!");
+            return Ok("You got the book back!");
         }
-        
     }
 }
---
+++ VVS-biblioteka/Controllers/LoanService.cs
@@ -0,0 +1,101 @@
+﻿using VVS_biblioteka.Models;
+
+namespace VVS_biblioteka.Controllers
+{
+    public interface ILoanService
+    {
+        void LoanBook(LoanRequest request, LibDbContext context);
+        void ReturnBook(GetBookBackRequest request, LibDbContext context);
+        object GetBookLoanDetails(int bookId, LibDbContext context);
+    }
+
+    public class LoanService : ILoanService
+    {
+        public void LoanBook(LoanRequest request, LibDbContext context)
+        {
+            Book book = context.Book.FirstOrDefault(b => b.Id == request.BookId);
+
+            var isBookLoaned = context.Loan.Any(l => l.BookId == book.Id);
+
+            if (isBookLoaned)
+            {
+                throw new ArgumentException("Book is already loaned!");
+            }
+
+            Loan loan = context.Loan.FirstOrDefault(l => l.UserId == request.UserId);
+
+            User user = context.User.FirstOrDefault(u => u.Id == request.UserId);
+
+            if (loan != null)
+            {
+                throw new ArgumentException("You already loaned a book!");
+            }
+
+            Loan newLoan = new()
+            {
+                BookId = request.BookId,
+                UserId = request.UserId,
+                Date = DateTime.Today,
+                Price = book.price,
+            };
+
+            ApplyCategorySpecificBenefits(user, newLoan);
+
+            context.Loan.Add(newLoan);
+            context.SaveChanges();
+        }
+
+        public void ReturnBook(GetBookBackRequest request, LibDbContext context)
+        {
+            Loan loan = context.Loan.FirstOrDefault(l => l.BookId == request.BookId);
+
+            if (loan == null)
+            {
+                throw new ArgumentException("Book with that id is not loaned!");
+            }
+
+            context.Loan.Remove(loan);
+            context.SaveChanges();
+        }
+
+        public object GetBookLoanDetails(int bookId, LibDbContext context)
+        {
+            var loan = context.Loan.FirstOrDefault(l => l.BookId == bookId);
+            var user = loan != null ? context.User.FirstOrDefault(u => u.Id == loan.UserId) : null;
+
+            return new
+            {
+                Loan = loan,
+                User = user
+            };
+        }
+
+        private void ApplyCategorySpecificBenefits(User user, Loan loan)
+        {
+            switch (user.UserType)
+            {
+                case UserType.Student:
+                    loan.Price = CalculateDiscountedFee(loan.Price, 30);
+                    loan.Days = 60;
+                    break;
+                case UserType.Ucenik:
+                    loan.Price = CalculateDiscountedFee(loan.Price, 10);
+                    loan.Days = 15;
+                    break;
+                case UserType.Penzioner:
+                    loan.Price = CalculateDiscountedFee(loan.Price, 15);
+                    loan.Days = 30;
+                    break;
+                case UserType.Dijete:
+                    loan.Price = CalculateDiscountedFee(loan.Price, 5);
+                    loan.Days = 10;
+                    break;
+            }
+        }
+
+        private decimal CalculateDiscountedFee(decimal baseFee, int discountPercentage)
+        {
+            return baseFee - (baseFee * discountPercentage / 100);
+        }
+    }
+}
