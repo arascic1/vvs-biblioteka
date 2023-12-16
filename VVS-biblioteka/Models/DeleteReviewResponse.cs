f128519193e5811715856654e01abb4d3088e2af
ehodzic <ehodzic6@etf.unsa.ba>
2023-12-16 14:28:57 +0100

    FInal

---
+++ VVS-biblioteka/Models/DeleteReviewResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace VVS_biblioteka.Models
+{
+    public class DeleteReviewResponse
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+    }
+}
---
+++ VVS-biblioteka/Models/GetAverageGradeResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace VVS_biblioteka.Models
+{
+    public class GetAverageGradeResponse
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public double Value { get; set; }
+    }
+}
