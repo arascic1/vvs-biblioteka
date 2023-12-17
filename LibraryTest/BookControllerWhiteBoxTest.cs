2f6a4a77f117c0b4afea33afcfa1e5f9d4ffed85
Danijel0 <dmiokovic1@etf.unsa.ba>
2023-12-17 12:40:34 +0100

    White Box testing za metodu GetBookDetails

--- LibraryTest/BookControllerWhiteBoxTest.cs
+++ LibraryTest/BookControllerWhiteBoxTest.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.AspNetCore.Routing;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VVS_biblioteka;
 using VVS_biblioteka.Controllers;
 using VVS_biblioteka.Models;
 
@@ -12,6 +15,20 @@ namespace LibraryTest
     [TestClass]
     public class BookControllerWhiteBoxTest
     {
+        private BookController bookController;
+        private LibDbContext _dbContext;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            var options = new DbContextOptionsBuilder<LibDbContext>()
+                .UseInMemoryDatabase(databaseName: "TestDatabase")
+                .Options;
+
+            bookController = new BookController(new LibDbContext(options));
+            _dbContext = new LibDbContext(options);
+        }
+
         [TestMethod]
         public void ApplyCategorySpecificBenefits_DiscountAppliedDaysUpdated_Student()
         {
@@ -101,7 +118,55 @@ namespace LibraryTest
             Assert.AreNotEqual(100, loan.Days); 
         }
 
+        [TestMethod]
+        public void GetBookDetails_BookNotFound()
+        {
+            var books = new List<Book>
+            {
+                new Book { Id = 1, Title="Test", Author="TestAuthor", Description="Description",price = 10},
+                new Book { Id = 2, Title="Test Two", Author="TestAuthorTwo",Description="Description",price=12},
+                new Book { Id = 3, Title="Test Three", Author="TestAuthorThree",Description="Description",price=45},
+            };
+            _dbContext.Book.AddRange(books);
+            _dbContext.SaveChanges();
+            var result = bookController.GetBookDetails(4);
+            Assert.IsInstanceOfType(result, typeof(NotFoundObjectResult));
+        }
+
+        [TestMethod]
+        public void GetBookDetails_ReturnsDetails()
+        {
+            var user = new User
+            {
+                Id = 1,
+                FirstName = "John",
+                LastName = "Doe",
+                Email = "john.doe@example.com",
+                UserType = UserType.Student,
+                PasswordHash = "null"
+            };
+            var loan = new Loan
+            {
+                 Id = 1, UserId = 1, BookId = 1, Date = DateTime.Now, Price = 12.55m, Days=21 
+            };
+            var books = new List<Book>
+            {
+                new Book { Id = 1, Title="Test", Author="TestAuthor", Description="Description",price = 10},
+                new Book { Id = 2, Title="Test Two", Author="TestAuthorTwo",Description="Description",price=12}
+            };
+            _dbContext.Book.AddRange(books);
+            _dbContext.Loan.Add(loan);
+            _dbContext.User.Add(user);
+            _dbContext.SaveChanges();
+            var result = bookController.GetBookDetails(1);
+            Assert.IsInstanceOfType(result, typeof(OkObjectResult));
+        }
 
 
+        [TestCleanup]
+        public void CleanUp()
+        {
+            _dbContext.Database.EnsureDeleted();
+        }
     }
 }
